c0445bd7b4503ebaa1f1cd866ab76d3b2466cb02
confh <onexite1@gmail.com>
2023-06-30 14:43:47 +0200

    Remove throw error in ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -6,8 +6,7 @@ module.exports = new Command({
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Shows the ping of the bot."),
-    async execute(interaction: any, client: CustomClient) {
-        throw new Error("test error");
+    async execute(interaction, client) {
         const date = interaction.createdTimestamp
         const message = await interaction.channel.send("Calculating latency")
         await message.delete()
@@ -16,7 +15,7 @@ module.exports = new Command({
             .setTitle("🏓 Pong!")
             .setFooter({ text: `${client.user?.username}`, iconURL: client.user?.avatarURL({ size: 1024 }) })
             .setDescription(`Latency: \`${Math.abs(message.createdTimestamp - date)}ms\`\nAPI Latency: \`${client.ws?.ping}ms\``)
-            .setColor("#313338")
+            .setColor(client.config2.colors.normal)
 
         await interaction.reply({ embeds: [embed] })
     }
